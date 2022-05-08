0ef48fc2f5dcd4e94ea536a1cf54e8adc95b4123
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-05-08 15:31:35 +0500

    fix

--- ts/types.ts
+++ ts/types.ts
@@ -6,3 +6,9 @@ let counter: number = 5;
 const arr: string[] = ["apple", "banana"];
 const num: number[] = [1, 2, 3];
 const bool: boolean[] = [true, false];
+
+const logger: (name: number) => void = (name: number) => {
+  console.log(name);
+};
+
+let numbe: number;
